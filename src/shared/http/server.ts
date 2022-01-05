f79bb3fddd233e062cf3c4c7b4f6c1052cabe0e6
elizandroDalastra <elizandro.dalastra@pampa.compasso>
2022-01-05 18:21:58 -0300

    config: setting typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import routes from '../routes';
+import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
@@ -11,7 +13,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (error: Error, request: Request, response: Response,  next: NextFunction ) => {
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
         status: 'error',
